7c65324260faa031de2bb9522dc9e62993f250e1
Ayush H <jqghebri@gmail.com>
2024-10-11 00:44:18 +0530

    creation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,14 +5,14 @@ import React from 'react'
 function NavBar() {
     return (
         <div className='flex'>
-            <div className='flex'>
+            <div className='flex gap-10 items-center'>
                 <Image 
                 src="/logo.png" 
                 alt='logo'
                 width={120}
                 height={60}
                 />
-                <div className='flex'>
+                <div className='flex gap-6 '>
                     <h2>Home</h2>
                     <h2>History</h2>
                     <h2>Help</h2>
