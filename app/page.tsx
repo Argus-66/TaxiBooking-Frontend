714e520fe05264bfe8ea9d5ea26dcd4b1f9da44f
Ayush H <jqghebri@gmail.com>
2024-10-16 22:03:30 +0530

    creation

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-3">
-
+        <div className="bg-blue-100">
+          Booking
+        </div>
+        <div className="col-span-2 bg-red-100">
+          Map
+        </div>
       </div>
     </div>
   );
