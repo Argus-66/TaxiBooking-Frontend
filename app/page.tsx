0d24f157b4f628692ff62ba5c2f969db3d111c7a
Ayush H <jqghebri@gmail.com>
2024-10-16 22:01:37 +0530

    creation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>check</h2>
+      <div className="grid grid-cols-3">
+
+      </div>
     </div>
   );
 }
