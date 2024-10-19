3f11dcd709c919707645a18b253a455b9f2396dd
Ayush H <jqghebri@gmail.com>
2024-10-19 18:06:20 +0530

    creation

--- components/Booking/AutoCompleteAddress.tsx
+++ components/Booking/AutoCompleteAddress.tsx
@@ -4,10 +4,17 @@ function AutoCompleteAddress() {
     return (
         <div className='mt-5'>
             <div>
-                <label >Where From?</label>
+                <label className='text-gray-400'>Where From?</label>
                 <input 
                 type="text" 
-                    className='bg-white p-1 border-[2px] w-full rounded-md outline-none'
+                    className='bg-white p-1 border-[2px] w-full rounded-md outline-none focus:border-yellow-300'
+                />
+            </div>
+            <div className='mt-3'>
+                <label className='text-gray-400'>Where To?</label>
+                <input 
+                type="text" 
+                    className='bg-white p-1 border-[2px] w-full rounded-md outline-none focus:border-yellow-300'
                 />
             </div>
         </div>
