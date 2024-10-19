4ed7c50d1e7843c38a1db7e95d7d3f0396d6d345
Ayush H <jqghebri@gmail.com>
2024-10-19 19:27:37 +0530

    creation

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="bg-blue-100">
+        <div className="">
           <Booking />
         </div>
         <div className="col-span-2 bg-red-100">
--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react';
 import AutoCompleteAddress from './AutoCompleteAddress'
 
 function Booking() {
+    const [screenHeight, setScreenHeight] = useState<number | null>(null);
+
+    useEffect(() => {
+        // Set the screen height when the component mounts on the client side
+        setScreenHeight(window.innerHeight * 0.72);
+
+        // Optional: Update height on window resize
+        const handleResize = () => setScreenHeight(window.innerHeight * 0.72);
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    if (screenHeight === null) return null;
+
     return (
         <div className='p-5'>
             <h2 className='text-[20px] font-semibold'>Booking</h2>
+            <div
+                className='border-[1px] p-5 rounded-md'
+                style={{height:screenHeight}}>
             <AutoCompleteAddress />
         </div>
+        </div >
     )
 }
 
