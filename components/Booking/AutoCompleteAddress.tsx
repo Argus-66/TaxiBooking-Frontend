8e7eb82acaa78b51cf1ef05d6f6ea6b16a634a6b
Ayush H <jqghebri@gmail.com>
2024-10-19 16:44:58 +0530

    creation

--- components/Booking/AutoCompleteAddress.tsx
+++ components/Booking/AutoCompleteAddress.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function AutoCompleteAddress() {
     return (
-        <div>AutoCompleteAddress</div>
+        <div className='mt-5'>
+            <div>
+                <label >Where From?</label>
+                <input 
+                type="text" 
+                    className='bg-white p-1 border-[2px] w-full rounded-md outline-none'
+                />
+            </div>
+        </div>
     )
 }
 
