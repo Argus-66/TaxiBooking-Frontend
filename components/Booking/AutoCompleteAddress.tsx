349a44b356486f7b3caf2d62641b5f7b09114296
Ayush H <jqghebri@gmail.com>
2024-11-01 14:13:30 +0530

    creation

--- components/Booking/AutoCompleteAddress.tsx
+++ components/Booking/AutoCompleteAddress.tsx
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from 'react'
 function AutoCompleteAddress() {
 
     const [source, setSource] = useState<any>()
-    const [addressList,setAddressList] = useState<any>([])
+    const [addressList, setAddressList] = useState<any>([])
 
     useEffect(() => {
-        getAddressList()
-    },[])
+        const delayDebounceFn = setTimeout(() => {
+            getAddressList()
+        }, 1000)
+        return () => clearTimeout(delayDebounceFn)
+    }, [source])
 
     const getAddressList = async () => {
         const res = await fetch('/api/search/address?q=' + source, {
@@ -27,8 +30,11 @@ function AutoCompleteAddress() {
                 <input
                     type="text"
                     className='bg-white p-1 border-[2px] w-full rounded-md outline-none focus:border-yellow-300'
+                    value = {source}
                     onChange={(e) => setSource(e.target.value)}
                 />
+
+                {addressList.suggestions.map((item:any,index:number))}
             </div>
             <div className='mt-3'>
                 <label className='text-gray-400'>Where To?</label>
